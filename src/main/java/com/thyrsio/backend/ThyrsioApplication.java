fb4f33599f300a3cd6064ebbe946500e991efb7f
Latorse Pierre <pierrelat33@gmail.com>
2026-09-22 11:50:42 +0200

    chore(init): initialize Spring Boot project with web dependency

---
+++ src/main/java/com/thyrsio/backend/ThyrsioApplication.java
@@ -0,0 +1,21 @@
+package com.thyrsio.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ThyrsioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThyrsioApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
