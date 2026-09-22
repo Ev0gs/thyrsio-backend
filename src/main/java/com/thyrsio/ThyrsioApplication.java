4ca3caf0a1fdd67845dea0c5a6524f1e2467d57f
Latorse Pierre <pierrelat33@gmail.com>
2026-09-22 12:05:19 +0200

    refactor: application folders and files structure

--- src/main/java/com/thyrsio/ThyrsioApplication.java
+++ src/main/java/com/thyrsio/ThyrsioApplication.java
@@ -1,5 +1,4 @@
-package com.thyrsio.backend;
-
+package com.thyrsio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class ThyrsioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThyrsioApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ThyrsioApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
--- src/test/java/com/thyrsio/ThyrsioApplicationTests.java
+++ src/test/java/com/thyrsio/ThyrsioApplicationTests.java
@@ -1,4 +1,4 @@
-package com.thyrsio.backend;
+package com.thyrsio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ThyrsioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
